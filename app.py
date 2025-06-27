8739f7c6a8f8db1708813a59a3be655d8eeaa1c9
naufalilhamfatikh <naufalilhamfatikh01@gmail.com>
2025-06-28 00:48:34 +0700

    add nltk download

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import seaborn as sns
 
 import nltk
 nltk.download('punkt', quiet=True)
+nltk.download('punkt_tab', quiet=True)
 nltk.download('stopwords', quiet=True)
 nltk.download('wordnet', quiet=True)
 nltk.download('omw-1.4', quiet=True)
